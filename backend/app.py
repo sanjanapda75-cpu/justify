494fa8f0799d2629b0887237296dbf20571ea8f9
Sanjana Panda <sanjana.pda75@gmail.com>
2026-02-03 10:05:04 +0000

    INSERT CASE DONE

--- backend/app.py
+++ backend/app.py
@@ -5,18 +5,18 @@ from db import ensure_schema
 
 from controllers.health_controller import health_bp
 from controllers.predict_controller import predict_bp
-# from controllers.cases_controller import cases_bp
+from controllers.cases_controller import cases_bp
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
 
-    # db_check()
+    
     ensure_schema()
 
     app.register_blueprint(health_bp)
     app.register_blueprint(predict_bp)
-    # app.register_blueprint(cases_bp)
+    app.register_blueprint(cases_bp)
 
     return app
 
